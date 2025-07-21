9456512d250913cf210afe9c6243daaf4592e02d
Nova <novacooksley@gmail.com>
2025-07-21 11:55:58 -0500

    Updated to GTK4

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -25,7 +25,6 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
             <Gtk.Calendar />
           </popover>
           <label label={time()} />
-          <Gtk.Video file={}></Gtk.Video>
         </menubutton>
       </centerbox>
     </window>
