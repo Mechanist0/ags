269f650931a68c8c9b693f8b24bd7df7e92523be
Nova <novacooksley@gmail.com>
2025-07-21 12:20:36 -0500

    Delete GTK3

--- app.ts
+++
@@ -1,8 +0,0 @@
-import app from "astal/gtk3/app";
-import "./src/vixpaper";
-import { startVixpaper, stopVixpaper } from "./src/vixpaper/vixpaperUtils";
-
-export const debug = true;
-var path = "./res/vixpaper/";
-
-startVixpaper(path);
