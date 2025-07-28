f698c31c16fa6c402fe0a265f3d0ba7c67edc5fb
Nova <novacooksley@gmail.com>
2025-07-28 15:07:36 -0500

    THE VIDEO WORKS

--- app.ts
+++ app.ts
@@ -2,8 +2,9 @@ import { App } from "astal/gtk4";
 import style from "./style.scss";
 import Bar from "./widget/Bar";
 import { startVixPaper } from "./src/vixpaper/VixPaper";
+import GLib from "gi://GLib?version=2.0";
 
-globalThis.resPath = "./res/wallpapers";
+globalThis.resPath = GLib.get_current_dir() + "/res/wallpapers/";
 globalThis.debug = true;
 
 startVixPaper(resPath);
--- src/vixpaper/VixPaper.tsx
+++ src/vixpaper/VixPaper.tsx
@@ -1,8 +1,20 @@
-import { App } from "astal/gtk4";
+import { App, Widget } from "astal/gtk4";
+import { Box, BoxProps, Button, Label } from "astal/gtk4/widget";
 import Astal from "gi://Astal?version=4.0";
 import Gdk from "gi://Gdk?version=4.0";
 import Gio from "gi://Gio?version=2.0";
 import Gtk from "gi://Gtk?version=4.0";
+import { interval } from "astal/time";
+import { GLib, signal } from "astal";
+
+const wallpaperPath = GLib.get_current_dir() + "/res/wallpapers/";
+
+const wallpaperPaths = [
+  wallpaperPath + "Flower0.jpg",
+  wallpaperPath + "Flower1.jpg",
+  wallpaperPath + "Flower2.jpg",
+  wallpaperPath + "crankshaft.mp4",
+];
 
 export const startVixPaper = (path: string) => {
   App.start({
@@ -22,6 +34,21 @@ export const stopVixPaper = () => {
 export const vixpaper = (monitor: Gdk.Monitor) => {
   const { TOP, LEFT, RIGHT, BOTTOM } = Astal.WindowAnchor;
   if (globalThis.debug) log(globalThis.resPath);
+
+  const boxEl: JSX.IntrinsicElements["box"] = Box({
+    valign: Gtk.Align.CENTER,
+    halign: Gtk.Align.CENTER,
+    children: [
+      Label({
+        label: "sup",
+      }),
+      Button({
+        onClicked: stopVixPaper,
+      }),
+      webmHandler(),
+    ],
+  });
+
   return (
     <window
       visible
@@ -31,15 +58,30 @@ export const vixpaper = (monitor: Gdk.Monitor) => {
       anchor={TOP | LEFT | RIGHT}
       application={App}
     >
-      <box valign={Gtk.Align.CENTER} halign={Gtk.Align.CENTER}>
-        <label label="Sup" />
-        <button onClicked={stopVixPaper} />
-      </box>
+      {boxEl}
     </window>
   );
 };
 
-// const webmHandler = () => {
-//   const mediaFile: Gstrea;
-//   snapshot.
-// }
+const animationHandle: Gtk.TickCallback = (
+  widget: Gtk.Widget,
+  clock: Gdk.FrameClock,
+): boolean => {
+  widget.queue_allocate();
+  return true;
+};
+
+const webmHandler = () => {
+  const testMedia = GLib.filename_from_utf8(
+    wallpaperPaths[3],
+    wallpaperPaths[3].length,
+  );
+  log(testMedia);
+  const video = Gtk.MediaFile.new_for_file(Gio.file_new_for_path(testMedia[0]));
+  const image = Gtk.Picture.new_for_paintable(video);
+  video.play();
+  video.set_loop(true);
+  image.add_tick_callback(animationHandle);
+
+  return image;
+};
