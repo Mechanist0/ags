a95531b5255d11f73ffb1d2ce52c1929e531bc6e
Nova <novacooksley@gmail.com>
2025-08-04 14:11:37 -0500

    Cleaned up app.ts

--- app.ts
+++ app.ts
@@ -1,10 +1,5 @@
-import Astal from "gi://Astal?version=4.0";
-import Gio from "gi://Gio?version=2.0";
-import GObject from "gi://GObject?version=2.0";
-import Gtk from "gi://Gtk?version=4.0";
-import { programInvocationName, programArgs } from "system";
-import { startVixPaper, vixpaper } from "./src/vixpaper/VixPaper";
 import { App } from "astal/gtk4";
+import { vixpaper } from "./src/vixpaper/VixPaper";
 
 App.start({
   main() {
